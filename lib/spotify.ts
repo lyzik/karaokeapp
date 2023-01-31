f51c3f72b63381e8d9ca26c4f46c986db25903fb
lyzik <mikolajlyzinski@gmail.com>
2023-01-31 23:23:17 +0100

    create project with search and auth

---
+++ lib/spotify.ts
@@ -0,0 +1,40 @@
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+const PLAYLISTS_ENDPOINT = 'https://api.spotify.com/v1/me/playlists';
+
+export const getAccessToken = async (refresh_token: any) => {
+    const response = await fetch(TOKEN_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${basic}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token,
+      }),
+    });
+
+    return response.json();
+};
+
+export const getUsersPlaylists = async (refresh_token : any) => {
+    const {access_token} = await getAccessToken(refresh_token);
+    return fetch(PLAYLISTS_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+};
+
+
+export const search = async (refresh_token : any, endpoint: string ) => {
+  const {access_token} = await getAccessToken(refresh_token);
+  return fetch(endpoint, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  })
+}
---
+++ pages/api/search.ts
@@ -0,0 +1,16 @@
+import { search } from '../../lib/spotify';
+import { getSession } from 'next-auth/react';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (req : NextApiRequest, res : NextApiResponse) => {
+  const {
+    token: {accessToken},
+  } : any = await getSession({req});
+  const query = req.query?.q;
+  const response = await search(accessToken, `https://api.spotify.com/v1/search?q=${query}&type=track&limit=10`);
+  const {tracks} = await response.json();
+
+  return res.status(200).json({tracks});
+};
+
+export default handler;
