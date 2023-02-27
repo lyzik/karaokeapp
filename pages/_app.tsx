2c3a86e6ef8867bc815f48c1e5903800c05b9be0
lyzik <mikolajlyzinski@gmail.com>
2023-02-27 18:22:29 +0100

    favicon, titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import GlobalStyles from '@/components/GlobalStyles';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 export default function App({Component, pageProps: {session, ...pageProps}} : AppProps) {
 
   return (
       <SessionProvider session={session}>
+      <Head>
+        <title>Let's Sing!</title>
+      </Head>
         <Component {...pageProps} />
         <GlobalStyles />
       </SessionProvider>
